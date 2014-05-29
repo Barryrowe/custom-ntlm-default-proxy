f95117690a0465525de099b82e06877adf3f985d
Barry Rowe <barryrowe@gmail.com>
2014-05-28 22:25:07 -0400

    Initial Commit

---
+++ CustomProxyAssembly/NTLMProxy.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace CustomProxyAssembly
+{
+    public class NTLMProxy : IWebProxy
+    {
+        public ICredentials Credentials
+        {
+            get
+            {      
+                //If recreating this from scratch, you need to import System.Configuration
+                //  in order to use the newer ConfigurationManager object.
+                string username = ConfigurationManager.AppSettings["proxyUsername"];
+                string password = ConfigurationManager.AppSettings["proxyPassword"];
+                string domain = ConfigurationManager.AppSettings["proxyDomain"];
+
+                NetworkCredential credential;
+                if (domain != null)
+                {
+                    credential = new NetworkCredential(username, password, domain);
+                }
+                else
+                {
+                    credential = new NetworkCredential(username, password);
+                }                
+                return credential;
+            }
+            set { }
+        }
+
+        public Uri GetProxy(Uri destination)
+        {
+            Uri proxy = null;
+
+            // Use the proxy server specified in AppSettings
+            string serverName = ConfigurationManager.AppSettings["proxyServer"];            
+            if (serverName != null)
+            {
+                proxy = new Uri(serverName);
+            }
+
+            return proxy;
+        }
+
+        public bool IsBypassed(Uri host)
+        {
+
+            string byPass = ConfigurationManager.AppSettings["proxyBypass"];
+            if (byPass != null)
+            {
+                string[] bypassUris = byPass.Split(',');
+
+                foreach (string bypassUri in bypassUris)
+                {
+                    if (host.AbsoluteUri.ToLower().Contains(bypassUri.Trim().ToLower()))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ CustomProxyAssemblyDemo/ProxyDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace CustomProxyAssemblyDemo
+{
+    //Run this Demo Project after updating the App.config with your environments
+    //  proxy settings to confirm your configuration settings are valid.
+    class ProxyDemo
+    {
+        static void Main(string[] args)
+        {
+            //This will make a simple webRequest, which will go through our NTLMProxy.
+            //  Any 3rd party library will also be routed through the NTLMProxy for
+            //  WebClient and/or WebRequest connections.
+            WebRequest webRequest = WebRequest.Create("http://pineapplepiranha.com/");
+            WebResponse webResp = webRequest.GetResponse();
+
+            System.Diagnostics.Debug.WriteLine(webResp.ContentType);
+            System.Diagnostics.Debug.WriteLine(webResp.ContentLength);
+
+            Console.Read();
+        }
+    }
+}
